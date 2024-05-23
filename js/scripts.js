436650a1d81911f77746c4cf0ac0c7ce1366e2c3
jsy5888 <oliviasunjin0318@gmail.com>
2024-05-23 19:39:29 -0400

    pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
 
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+
+let pokemon1 = {name :"Bulbasaur", height:7, types: ['grass','posion'] }
+let pokemon2 = {name :"Charmander", height:6, types: 'fire' }
+let pokemon3 = {name :"Squirtle", height:5, types: 'water' }
+
+
+//array of pokemon
+let pokemonList = [pokemon1,pokemon2,pokemon3]
