cf6ce49035d99682ae592932b9bfbbfea2ce6d1a
jsy5888 <oliviasunjin0318@gmail.com>
2024-05-28 18:17:44 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,16 @@ let pokemon3 = {name :"Squirtle", height:5, types: 'water' }
 
 
 //array of pokemon
-let pokemonList = [pokemon1,pokemon2,pokemon3]
+let pokemonList = [
+    {name :"Bulbasaur", height:7, types: ['grass','posion']},
+    {name :"Charmander", height:6, types: 'fire' },
+    {name :"Squirtle", height:5, types: 'water' }
+];
+
+for (let i=0; i<pokemonList.length; i++){
+    if (pokemonList[i].height<6){
+        console.log(pokemonList[i].name + " (height:7)");
+    }else if (pokemonList[i].height=6){
+        console.log(pokemonList[i].name + " (height:6)");
+    }else {
+        console.log(pokemonList[i].name + " (height:5)");}
