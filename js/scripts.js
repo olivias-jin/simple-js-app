9477c927a231ce92e8ab5962d1b2a51a1628678f
jsy5888 <oliviasunjin0318@gmail.com>
2024-05-29 20:11:01 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
-
-
 // let pokemon1 = {name :"Bulbasaur", height:7, types: ['grass','posion'] }
 // let pokemon2 = {name :"Charmander", height:6, types: 'fire' }
 // let pokemon3 = {name :"Squirtle", height:5, types: 'water' }
 
-
-//array of pokemon
-let pokemonList = [
-    {name :"Bulbasaur", height:7, types: ['grass','posion']},
-    {name :"Charmander", height:6, types: 'fire' },
-    {name :"Squirtle", height:5, types: 'water' }
-];
+// let pokemonList = [
+//     {name :"Bulbasaur", height:7, types: ['grass','posion']},
+//     {name :"Charmander", height:6, types: 'fire' },
+//     {name :"Squirtle", height:5, types: 'water' }
+// ];
 
 // for (let i=0; i<pokemonList.length; i++){
 //     if (pokemonList[i].height>6){
@@ -21,6 +17,15 @@ let pokemonList = [
 //         console.log(pokemonList[i].name + " (height:6)");}
 //     }
 
+//array of pokemon
+// IIFE 
+(function () { 
+    let pokemonList = [
+    {name :"Bulbasaur", height:7, types: ['grass','posion']},
+    {name :"Charmander", height:6, types: 'fire' },
+    {name :"Squirtle", height:5, types: 'water' }]
+
+});
 
 // exercise 1.5 forEach()
 pokemonList.forEach(function(user){
